27bd9404aba4701cfe436aac1cc476ab3c90b2b8
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2023-11-11 01:31:15 +0800

    Main page

--- pages/components/MainNavBar.jsx
+++ pages/components/MainNavBar.jsx
@@ -2,6 +2,7 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default () => {
   return (
@@ -11,9 +12,9 @@ export default () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+            <Nav.Link href="#home">課程</Nav.Link>
+            <Nav.Link href="#link">登入</Nav.Link>
+            {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">
                 Another action
@@ -23,7 +24,7 @@ export default () => {
               <NavDropdown.Item href="#action/3.4">
                 Separated link
               </NavDropdown.Item>
-            </NavDropdown>
+            </NavDropdown> */}
           </Nav>
         </Navbar.Collapse>
       </Container>
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container } from "react-bootstrap";
 import Link from "next/link";
 import MainNavbar from "./components/MainNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -6,7 +6,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 export default function HomePage() {
   return (
     <>
-    <MainNavbar />
+      <MainNavbar />
+      <Container>
+        {/* 聖哲的官網 */}
+        <Container>
+          {/* 公告 */}
+          {/* 課堂規範 */}
+        </Container>
+      </Container>
     </>
   );
 }
