3dd26a93cb504452cd783787a99efad4690b4a68
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2023-11-11 15:06:20 +0800

    main page update

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,21 +1,35 @@
-import { Form, Button, Container } from "react-bootstrap";
+import {
+  Form,
+  Button,
+  Container,
+  ListGroup,
+  ListGroupItem,
+  Card,
+} from "react-bootstrap";
 import Link from "next/link";
 import MainNavbar from "./components/MainNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Announcement() {
-  return <Container></Container>;
+  return (
+    <Card style={{ width: "40%" }}>
+      <Card.Header className="text-center">公告</Card.Header>
+    </Card>
+  );
 }
 
 function Rules() {
-  return(
-  <Container>
-  <h1 className="text-center">課堂規範</h1>
-  <div className="w-100"></div>
-  <h1 className="text-center">1. 不要睡覺</h1>
-  <h1 className="text-center">2. 不要聊天</h1>
-  <h1 className="text-center">3. 不會的要舉手發問</h1>
-  </Container>
+  return (
+    <Card style={{ width: "40%" }}>
+      <Card.Header className="text-center">課堂規範</Card.Header>
+      <Card.Body>
+        <ListGroup style={{ border: "none" }}>
+          <ListGroupItem>1. 不要睡覺</ListGroupItem>
+          <ListGroupItem>2. 不要聊天</ListGroupItem>
+          <ListGroupItem>3. 不會的要舉手發問</ListGroupItem>
+        </ListGroup>
+      </Card.Body>
+    </Card>
   );
 }
 
@@ -23,10 +37,10 @@ export default function HomePage() {
   return (
     <>
       <MainNavbar />
-      <Container>
+      <Container className="mt-3">
         {/* 聖哲的官網 */}
         <h1 className="text-center">聖哲老師的班級網站</h1>
-        <Container className="d-flex flex-row">
+        <Container className="d-flex justify-content-around mt-5">
           <Announcement />
           <Rules />
         </Container>
