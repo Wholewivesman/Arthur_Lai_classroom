8f553b175eafbb7b71f3aca8d491f2174d83bf12
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2023-11-11 14:33:06 +0800

    update

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,17 +3,24 @@ import Link from "next/link";
 import MainNavbar from "./components/MainNavbar";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+function Announcement() {
+  return <Container></Container>;
+}
+
+function Rules() {
+  return <Container></Container>;
+}
+
 export default function HomePage() {
   return (
     <>
-      <MainNavbar>
-      </ MainNavbar>
+      <MainNavbar />
       <Container>
         {/* 聖哲的官網 */}
         <h1 className="text-center">聖哲老師的班級網站</h1>
-        <Container>
-          {/* 公告 */}
-          {/* 課堂規範 */}
+        <Container className="d-flex flex-row">
+          <Announcement />
+          <Rules />
         </Container>
       </Container>
     </>
