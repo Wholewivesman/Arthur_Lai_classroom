5d2c3eb07c06f47d8f09050de3c655224a069f9c
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2023-11-12 23:00:33 +0800

    Update MainNavbar

--- pages/components/MainNavbar.jsx
+++ pages/components/MainNavbar.jsx
@@ -3,15 +3,7 @@ import { useEffect, useState } from "react";
 import { verify } from "../client/token";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-export default function MainNavbar() {
-  let authed = false;
-  const [loggedIn, setLoggedIn] = useState(false);
-  useEffect(() => {
-    if (!authed) {
-      verify().then((success) => setLoggedIn(success));
-      authed = true;
-    }
-  }, [authed]);
+function BeforeLoggedIn() {
   return (
     <Navbar
       expand="lg"
@@ -27,32 +19,73 @@ export default function MainNavbar() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="nav navbar-nav ms-auto">
-            {loggedIn ? (
-              <NavDropdown
-                style={{ fontSize: "20px" }}
-                title="課程"
-                id="basic-nav-dropdown"
-              >
-                <NavDropdown.Item href="#action/3.1">數學課</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">國語課</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">英文課</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">自然課</NavDropdown.Item>
-              </NavDropdown>
-            ) : (
-              <Nav.Link
-                style={{ fontSize: "20px" }}
-                className="active"
-                href="/Login"
-              >
-                <b>
-                  <font face="UD Digi Kyokasho NK-B">帳號登入</font>
-                </b>
-              </Nav.Link>
-            )}
+            <Nav.Link
+              style={{ fontSize: "20px" }}
+              className="active"
+              href="/Login"
+            >
+              <b>
+                <font face="UD Digi Kyokasho NK-B">帳號登入</font>
+              </b>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
+}
+
+function AfterLoggedIn() {
+  return (
+    <Navbar
+      expand="lg"
+      className="navbar navbar-light"
+      style={{ backgroundColor: "#e3f2fd" }}
+    >
+      <Container>
+        <Navbar.Brand style={{ fontSize: "40px" }} href="/">
+          <b>
+            <font face="UD Digi Kyokasho NK-B">聖哲老師的學習網站</font>
+          </b>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="nav navbar-nav ms-auto">
+            <NavDropdown
+              style={{ fontSize: "20px" }}
+              title="課程"
+              id="basic-nav-dropdown"
+            >
+              <NavDropdown.Item href="#action/3.1">數學課</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">國語課</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">英文課</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">自然課</NavDropdown.Item>
+            </NavDropdown>
+            <Nav.Link
+              style={{ fontSize: "20px" }}
+              className="active"
+              href="/Login"
+            >
+              <b>
+                <font face="UD Digi Kyokasho NK-B">帳號</font>
+              </b>
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default function MainNavbar() {
+  let authed = false;
+  const [loggedIn, setLoggedIn] = useState(false);
+  useEffect(() => {
+    if (!authed) {
+      verify().then((success) => setLoggedIn(success));
+      authed = true;
+    }
+  }, [authed]);
+  return <>{loggedIn ? <BeforeLoggedIn /> : <AfterLoggedIn />}</>;
 }
