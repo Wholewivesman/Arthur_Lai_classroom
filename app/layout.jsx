4830db92b4d8f7cb16f7a0d59f783ceeee709a25
Pc89752 <74953956+Pc89752@users.noreply.github.com>
2024-03-16 16:00:36 +0800

    Fix authentication

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,33 +3,34 @@
 import { useEffect, useRef, useState } from "react";
 import MainNavbar from "./components/MainNavbar";
 import { UserContext } from "./contexts/UserContext";
-import { User } from "./class/user";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function RootLayout({ children }) {
-  const [isAuthed, setIsAuthed] = useState(false);
-  const [userProfile, setUserProfile] = useState();
+  const authRef = useRef(false);
+  const [userProfile, setUserProfile] = useState({});
   useEffect(() => {
-    if (!isAuthed) {
+    if (!authRef.current && Object.keys(userProfile).length === 0) {
+      authRef.current = true;
+      console.log("auth starting");
       fetch("/api/auth")
         .then((res) => res.json())
         .then((data) => {
-          setIsAuthed(true);
-          if (data.success) {
-            setUserProfile(data.decodedPayload);
-          }
+          console.log(data.success);
+          if (data.success) setUserProfile(data.decodedPayload);
+          else setUserProfile({});
         })
         .catch((err) => console.error(err));
+      console.log("auth end");
     }
-  }, [isAuthed, userProfile]);
+  }, [userProfile]);
 
   return (
     <html>
       <head>
         <title>聖哲老師的學習網站</title>
       </head>
-      <body className="mb-5">
-        {isAuthed && (
+      <body className="mb-5" style={{ minHeight: "110vh" }}>
+        {authRef.current && (
           <UserContext.Provider value={userProfile}>
             <MainNavbar />
             {children}
